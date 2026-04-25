765ff22b7a2fc7c79b6630f44713f824657ed3d5
Jigar Gosar <jigar.gosar@gmail.com>
2026-04-25 09:38:36 +0530

    add webExt configuration for Chromium profile in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'wxt';
+import {resolve} from 'node:path';
+import {defineConfig} from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-solid'],
+    modules: ['@wxt-dev/module-solid'],
+    webExt: {
+        chromiumProfile: resolve('.wxt/chrome-data'),
+        keepProfileChanges: true,
+    },
 });
